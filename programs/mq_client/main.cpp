b36b6da187b552c6f513fee56d8a9da37054a8d6
Steve Gerbino <steve@gerbino.co>
2021-03-09 03:44:36 -0500

    #4: Clean up dependencies

--- programs/mq_client/main.cpp
+++ programs/mq_client/main.cpp
@@ -11,20 +11,14 @@ using namespace koinos;
 
 int main( int argc, char** argv )
 {
-   string amqp_url;
-   bool broadcast_mode;
-   string content_type;
-   string routing_key;
-   string payload;
-
    program_options::options_description desc( "Koinos MQ Client options");
    desc.add_options()
       ("help,h", "print usage message")
-      ("amqp,a", program_options::value( &amqp_url )->default_value( "amqp://guest:guest@localhost:5672/" ), "amqp url")
-      ("broadcast,b", program_options::value( &broadcast_mode )->default_value( false ), "broadcast mode")
-      ("content-type,c", program_options::value( &content_type )->default_value( "application/json" ), "content type of the message")
-      ("routing-key,r", program_options::value( &routing_key )->default_value( "" ), "routing key of the message")
-      ("payload,p", program_options::value( &payload )->default_value( "" ), "payload of the message")
+      ("amqp,a", program_options::value< string >()->default_value( "amqp://guest:guest@localhost:5672/" ), "amqp url")
+      ("broadcast,b", program_options::value< bool >()->default_value( false ), "broadcast mode")
+      ("content-type,c", program_options::value< string >()->default_value( "application/json" ), "content type of the message")
+      ("routing-key,r", program_options::value< string >()->default_value( "" ), "routing key of the message")
+      ("payload,p", program_options::value< string >()->default_value( "" ), "payload of the message")
       ;
 
    program_options::variables_map vm;
@@ -36,7 +30,14 @@ int main( int argc, char** argv )
       return EXIT_SUCCESS;
    }
 
+   string amqp_url     = vm[ "amqp" ].as< string >();
+   bool broadcast_mode = vm[ "broadcast" ].as< bool >();
+   string content_type = vm[ "content-type" ].as< string >();
+   string routing_key  = vm[ "routing-key" ].as< string >();
+   string payload      = vm[ "payload" ].as< string >();
+
    mq::client c;
+
    if( c.connect( amqp_url ) != mq::error_code::success )
       return EXIT_FAILURE;
 
